beede8cf86f61d694eae53a6daa390c3c744f506
leandromoren <leandro.moren@outlook.com>
2024-01-09 21:08:15 -0300

    commit

--- lib/utils/constants/text_strings.dart
+++ lib/utils/constants/text_strings.dart
@@ -4,17 +4,17 @@ class TTexts {
 
   // -- Oboarding texts
   static const String onBoardingTitle1 = 'Utiliza el boton para pedir auxilio';
-  static const String onBoardingTitle2 = 'Carga tus datos medicos en la carta';
-  static const String onBoardingTitle3 = 'Comparte tu ubicacion con tus familiares';
+  static const String onBoardingTitle2 = 'Contamos con un mapa de tu ciudad';
+  static const String onBoardingTitle3 = 'Ubicacion en tiempo real';
 
   static const String onBoardSubTitle1 = 'Mantene presionado el boton rojo para llamar inmediantamente al 911 (policia), explica con detalles tu situacion.'; 
-  static const String onBoardSubTitle2 = 'Cargar tus datos medicos en la carta sera facil para tus familiares y terceros.'; 
-  static const String onBoardSubTitle3 = 'Compartir tu ubicacion facilitara la busqueda de ayuda mas cercana.';
+  static const String onBoardSubTitle2 = 'Busca las estaciones policiales mas cercanas utilizando el mapa'; 
+  static const String onBoardSubTitle3 = 'Comparte tu ubicacion con tus amigos o familia';
 
   // -- Texto home page
   static const String tituloPrincipalHome = 'Llama a emergencias presionando el boton';
   static const String tituloSecundarioHome = 'Mantente a salvo en todo momento';
-  static const String tituloAyudame = "Ayudame!";
+  //static const String tituloAyudame = "Ayudame!";
 
   // -- Mensajes de error
   static const String errorLlamarPolicia = 'Lo sentimos, ha ocurrido un error al ejecutar el llamado. Cierre la aplicacion y vuelva a intentarlo.';
