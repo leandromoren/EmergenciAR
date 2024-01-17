b21c2a6085631da21c8f80c4b4e2babe1192632d
leandromoren <leandro.moren@outlook.com>
2024-01-17 10:01:29 -0300

    commit

--- lib/features/pages/home_page.dart
+++ lib/features/pages/home_page.dart
@@ -1,7 +1,4 @@
 // ignore_for_file: sized_box_for_whitespace, prefer_const_constructors, sort_child_properties_last
-
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:vidar_app/features/pages/widgets/compartir_ubicacion.dart';
 import 'package:vidar_app/features/pages/widgets/help_button.dart';
